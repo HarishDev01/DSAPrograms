cb5ccaa212218d71791d6fcf93717246df2da7c6
harish <harishravi.softeon@gmail.com>
2025-03-22 14:20:55 +0530

    DSA first program using Node

---
+++ src/NodeProgram.java
@@ -0,0 +1,14 @@
+public class NodeProgram {
+
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.add(9);
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(7);
+
+       linkedList.printValues();
+    }
+}
